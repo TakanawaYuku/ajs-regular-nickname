0616eb0433683577f95e06c52f82481f65e58578
TakanawaYuku <yukutakanawa@gmail.com>
2023-03-05 16:21:33 +0300

    update code regular

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,42 +1,21 @@
 import Validator from '../app';
 
-test.each(['1test', '_test', '-test', 'test1', 'test_', 'test-', 'te#st', 'te st', 't1234t', 't12345t',
-])(
-  ('The method must return false'),
-  (name) => {
-    expect(Validator.validateUsername(name)).toBeFalsy();
-  },
-);
-
-test.each(['1test', '_test', '-test', 'test1', 'test_', 'test-',
-])(
-  ('The method must return message \'Имя не должно начинаться и заканчиваться цифрами, символами подчёркивания или тире\''),
-  (name) => {
-    expect(Validator.validateUsername(name, true)).toBe('Имя не должно начинаться и заканчиваться цифрами, символами подчёркивания или тире');
-  },
-);
-
-test.each(['te#st', 'te st',
-])(
-  ('The method must return message \'Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)\''),
-  (name) => {
-    expect(Validator.validateUsername(name, true)).toBe('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
-  },
-);
-
-test.each(['t1234t', 't12345t',
-])(
-  ('The method must return message \'Имя не должно содержать подряд более трёх цифр\''),
-  (name) => {
-    expect(Validator.validateUsername(name, true)).toBe('Имя не должно содержать подряд более трёх цифр');
-  },
-);
-
-test.each(['te1st', 'te123st', 'te12-34st', 'te12_34st',
-])(
-  ('The method must return true or message \'ok\''),
-  (name) => {
-    expect(Validator.validateUsername(name)).toBeTruthy();
-    expect(Validator.validateUsername(name, true)).toBe('ok');
-  },
-);
+test.each([
+  ['Yuku', true],
+  ['Takanawa', true],
+  ['Bukuska92nick', true],
+  ['Dreiko-yuku', true],
+])('nickname valid', (name, expected) => {
+  const nickname = new Validator(name);
+  expect(nickname.validateUsername()).toBe(expected);
+});
+test.each([
+  ['4yukutakanawa', false],
+  ['Bukuska1992', false],
+  ['2306Nick', false],
+  ['Yuku19922306Takanawa', false],
+  ['yuku*bukuska', false],
+])('nickname invalid', (name, expected) => {
+  const nickname = new Validator(name);
+  expect(nickname.validateUsername()).toBe(expected);
+});
--- src/app.js
+++ src/app.js
@@ -1,20 +1,9 @@
 export default class Validator {
-  static validateUsername(nameParam, message = false) {
-    let name = nameParam;
-    if (/^[\d|_|-]/.test(name) || /[\d|_|-]$/.test(name)) return message ? 'Имя не должно начинаться и заканчиваться цифрами, символами подчёркивания или тире' : false;
+  constructor(name) {
+    this.name = name;
+  }
 
-    name = name.replace(/-/g, 'q');
-
-    if (/\W/.test(name)) return message ? 'Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)' : false;
-
-    if (/\d{4}/.test(name)) return message ? 'Имя не должно содержать подряд более трёх цифр' : false;
-
-    return message ? 'ok' : true;
+  validateUsername() {
+    return /^[a-z][-\w]*[a-z]$/i.test(this.name) && !(/\d{4,}/.test(this.name));
   }
 }
-
-// Для проверки
-const names = ['Yuku', 'Takanawa', 'Bukuska', 'bukuska1992', '_Nick', 'Nick2306w'];
-
-// eslint-disable-next-line no-console
-names.forEach((name) => console.log(name, Validator.validateUsername(name, true)));
